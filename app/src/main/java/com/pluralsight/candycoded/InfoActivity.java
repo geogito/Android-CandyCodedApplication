c48955d32cc183bd0887f987d0f042a675b8803e
Geogito <geoffreygit@gmail.com>
2020-10-15 23:03:29 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,11 +1,15 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import cz.msebera.android.httpclient.client.utils.URIUtils;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -22,7 +26,14 @@ public class InfoActivity extends AppCompatActivity {
 
 
     }
-
+    public void createMapIntent(View view)  {
+        Uri uriAddess = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801".);
+         Intent mapintent = new Intent(Intent.Action_view, uriAddess );
+         mapintent.setPackage("com.google.android.apps.maps".);
+         if(mapintent.resolveActivity(getPackageManager() ) !=null){
+             startActivity(mapintent);
+         }
+    }
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***

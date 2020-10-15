62cc44848fe21b4a3fde5c8860467c18672a256f
Geogito <geoffreygit@gmail.com>
2020-10-15 23:33:17 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,7 +36,7 @@ public class InfoActivity extends AppCompatActivity {
     }
     public void createPhoneIntent(View view) {
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
-        phoneIntent.setData(Url.parse(tel:0123456789));
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
         startActivity(phoneIntent);
     }
     // ***

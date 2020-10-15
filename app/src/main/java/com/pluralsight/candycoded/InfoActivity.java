c1372a1fb80be70372a1e623e28abd907408a630
Geogito <geoffreygit@gmail.com>
2020-10-15 23:27:55 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -34,6 +34,11 @@ public class InfoActivity extends AppCompatActivity {
              startActivity(mapintent);
          }
     }
+    public void createPhoneIntent(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Url.parse(tel:0123456789));
+        startActivity(phoneIntent);
+    }
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
